b819ad4e36fa7f6739c777c6233689f167ff45f0
raphaelsty <raphael.sourty@gmail.com>
2020-04-20 01:42:50 +0200

    remove app

--- app/app.py
+++
@@ -1,6 +0,0 @@
-from .__init__ import create_app
-
-if __name__ == '__main__':
-    app = create_app()
-    # Threaded option to enable multiple instances for multiple user access support
-    app.run(threaded=True, port=5000)
