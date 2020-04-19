83c45cf8e83b864a01c892bf1d97568ae1e2cf18
raphaelsty <raphael.sourty@gmail.com>
2020-04-20 01:25:04 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,6 @@
+from .__init__ import create_app
+
 if __name__ == '__main__':
-    from .__init__ import create_app
     app = create_app()
     # Threaded option to enable multiple instances for multiple user access support
     app.run(threaded=True, port=5000)
